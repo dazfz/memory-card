e67dab97a15d97bb760bc6f9ed67eedb739047c4
dazfz <dazhi891@gmail.com>
2023-05-13 12:28:22 -0400

    rdy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Center } from "@chakra-ui/react";
+import Game from "./components/Game";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center>
+      <Box
+        w="30%"
+        height="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Game />
+      </Box>
+    </Center>
   );
-}
+};
 
 export default App;
